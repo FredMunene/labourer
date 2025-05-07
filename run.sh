b35313553d7aae9bf541dc5b0cb812ff72b8624c
khahussein <kherld.hussein@gmail.com>
2025-05-07 11:12:31 +0300

    update script to run docker

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@
 chmod +x run.sh
 
 # Build and start the containers
-docker-compose up --build
+docker build -t labourer_web .
+docker run -d -p 8000:8000 --name labourer_web labourer_web
+
+echo "Site running at: http://localhost:8000/"
 
 # To stop the containers, press Ctrl+C 
